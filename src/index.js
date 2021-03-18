6045e357f33c3c239c640231dcb6041524befb2d
dkstjdwns114 <sungjoon256@gmail.com>
2021-03-18 16:55:12 +0900

    주석 추가

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import axios from "axios";
 
-axios.defaults.baseURL =
-  process.env.NODE_ENV === "development" ? "/" : "http://52.79.102.189:8080/";
+// local에서 실행할땐 주석 해제
+// axios.defaults.baseURL =
+//   process.env.NODE_ENV === "development" ? "/" : "http://52.79.102.189:8080/";
 
 ReactDOM.render(<App />, document.getElementById("app"));
