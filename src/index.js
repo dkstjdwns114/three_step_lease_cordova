1c6e1aab42440f41bdfc8ac4d8bb50954555c673
dkstjdwns114 <sungjoon256@gmail.com>
2021-04-05 22:04:04 +0900

    테마 변경

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from "./App";
 import axios from "axios";
 
 // local에서 실행할땐 주석 해제
-// axios.defaults.baseURL =
-//   process.env.NODE_ENV === "development" ? "/" : "http://52.79.102.189:8080/";
+axios.defaults.baseURL =
+  process.env.NODE_ENV === "development" ? "/" : "http://52.79.102.189:8080/";
 
-ReactDOM.render(<App />, document.getElementById("app"));
+ReactDOM.render(<App />, document.getElementById("root"));
